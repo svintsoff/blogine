8565a06e5ddb00aa75895b8cf859dc82b67ec504
svintsoff <svintsoff.ya@yandex.ru>
2023-02-18 02:54:25 +0400

    Feat: Api routing v1 for Articles; Article CRUD

---
+++ app/Http/Controllers/Api/v1/ArticleController.php
@@ -0,0 +1,87 @@
+<?php
+
+namespace App\Http\Controllers\Api\v1;
+
+use App\Http\Controllers\Controller;
+use App\Models\Article;
+use Illuminate\Http\JsonResponse;
+use Illuminate\Http\Request;
+
+class ArticleController extends Controller
+{
+    public function index(): JsonResponse
+    {
+        // TODO: Pagination by 10
+
+        $articles = Article::all();
+
+        $result = [
+            'status' => 'success',
+            'data' => $articles
+        ];
+
+        return response()
+            ->json($result);
+    }
+
+    public function store(Request $request): JsonResponse
+    {
+        $article = new Article;
+
+        $article->title = $request->input('title');
+        $article->text  = $request->input('text');
+
+        $article->save();
+
+        $result = [
+            'status' => 'success',
+        ];
+
+        return response()
+            ->json($result);
+    }
+
+    public function show($article): JsonResponse
+    {
+        $data = Article::find($article);
+
+        $result = [
+            'status' => 'success',
+            'data' => $data
+        ];
+
+        return response()
+            ->json($result);
+    }
+
+    public function update(Request $request, $article): JsonResponse
+    {
+        $data = Article::find($article);
+
+        $data->title = $request->input('title');
+        $data->text  = $request->input('text');
+
+        $data->save();
+
+        $result = [
+            'status' => 'success'
+        ];
+
+        return response()
+            ->json($result);
+    }
+
+    public function destroy($article): JsonResponse
+    {
+        $data = Article::find($article);
+
+        $data->delete();
+
+        $result = [
+            'status' => 'success'
+        ];
+
+        return response()
+            ->json($result);
+    }
+}
